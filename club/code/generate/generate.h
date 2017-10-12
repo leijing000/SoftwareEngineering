95eb60a09517db1cdcac91f8eded6179788297d0
leijing000 <1084961608@qq.com>
2017-10-12 21:32:13 +0800

    Delete generate.h

--- club/code/generate/generate.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include<iostream>
-#include<string>
-
-using namespace std;
-
-void generate(int depart_num, int stu_num);                   //随机生成函数
-void print_generate_information(int depart_num, int stu_num); //输出随机生成的部门信息和学生信息
